b5ae2e1126635c9f7cd1196ed59789318d8ec26c
Rafael <rafaelsilva@northnerds.com>
2024-10-29 14:37:43 -0700

    initial plan

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+WaiterAI 
+
+Features:
+- Interperate inputted menu
+- Take orders from customers
+- Send orders to the kitchen
+- Send notifications to customers when their order or table is ready
+
+"""
