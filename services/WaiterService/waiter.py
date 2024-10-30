4942c0a8a749a84d348c30f44ef1fc0e00da4919
Rafael <rafaelsilva@northnerds.com>
2024-10-29 23:13:39 -0700

    add TODO

--- services/WaiterService/waiter.py
+++ services/WaiterService/waiter.py
@@ -8,6 +8,7 @@ class WaiterAI:
     def __init__(self):
         self.client = anthropic.Client(api_key=os.getenv("ANTHROPIC_API_KEY"))
 
+    # TODO: Add prompt types for different types of prompts (orders, questions, etc.)
     def prompt(self, system_prompt, user_prompt):
         message = self.client.messages.create(
             model="claude-3-5-sonnet-20241022",
