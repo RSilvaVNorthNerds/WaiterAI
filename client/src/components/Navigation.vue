f1778a8382d0de9e3d29785677e4add876804171
Rafael <rafaelsilva@northnerds.com>
2024-11-19 18:04:47 -0800

    waiterAI logo

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -1,9 +1,7 @@
 <template>
   <nav class="main-nav">
-    <ul>
-      <img src="../assets/images/vue.svg" alt="logo" />
-      WaiterAI
-    </ul>
+    <img src="../assets/images/waiterAI_logo.svg" class="nav-logo" alt="logo" />
+    <h1>WaiterAI</h1>
   </nav>
 </template>
 
@@ -11,9 +9,20 @@
 .main-nav {
   width: "100%";
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   padding: 1rem;
   background-color: #181818;
+}
+
+.main-nav h1 {
+  color: white;
+  margin: 0;
+}
+
+.nav-logo {
+  width: 100px;
+  height: 100px;
+  margin-right: 1rem;
 }
 </style>
